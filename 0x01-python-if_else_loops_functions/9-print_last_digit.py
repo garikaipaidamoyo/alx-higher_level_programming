eed7ac7c0e9e1db6ca32f8c8c9a6cd2b54ad82c0
paidamoyo <garikaipaidamoyo@gmail.com>
2023-03-08 09:40:31 -0800

    There are only 3 colors, 10 digits, and 7 notes; it's what we do with them that's important

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -3,6 +3,7 @@ def print_last_digit(number):
     """
     a function that prints the last digit
     """
+    number = abs(number)
     last_digit = number % 10
-    print(last_digit)
+    print(last_digit, end='')
     return last_digit
