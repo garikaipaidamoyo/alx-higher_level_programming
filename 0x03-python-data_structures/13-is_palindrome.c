01ee92cba9a6e015a082b10923a89aff63bf2f26
paidamoyo <garikaipaidamoyo@gmail.com>
2023-03-13 07:39:43 -0700

    Linked list palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,94 +1,61 @@
 #include "lists.h"
 
-/**
- * _strlen_recursion - returns length of a string
- * @s: string to be assessed
- * Return: length of string
- */
-
-int _strlen_recursion(char *s)
-{
-	if (*s == '\0')
-		return (0);
-	return (_strlen_recursion(s + 1) + 1);
-}
+listint_t *reverse_listint(listint_t **head);
 
 /**
- * check_palin - check if string is a palindrome
- * @s: string to be assessed
- * @start: first index
- * @end: end index
- * Return: 1 if palindrome, 0 if not palindrome
- */
-
-int check_palin(char *s, int start, int end)
-{
-	if (s[start] != s[end])
-		return (0);
-	if (start >= end)
-		return (1);
-	return (check_palin(s, start + 1, end - 1));
-}
-
-/**
- * is_palin - determine if string is a palindrome
- * @s: string to be assessed
- * Return: 1 if string is palindrome and 0 if not
- */
-
-int is_palin(char *s)
-{
-	int end;
-
-	if (*s == '\0')
-		return (1);
-	end = _strlen_recursion(s) - 1;
-	return (check_palin(s, 0, end));
-}
-
-/**
- * is_palindrome - check if a singly linked list is a palindrome
+ * is_palindrome - Write a function in C that checks if a singly linked
+ *                 list is a palindrome.
  *
- * @head: start of linked list
- * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ * @head: This is the input single linked list
+ *
+ * Return: Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ *                 An empty list is considered a palindrome
+ *
  */
 
 int is_palindrome(listint_t **head)
 {
-	listint_t *temp = NULL;
-	listint_t *p = NULL;
-	char *s;
-	int numNodes = 0;
-	int i = 0;
+	listint_t *aux = *head, *current = *head;
 
-	if (*head == NULL || head == NULL)
+	if (*head == NULL)
 		return (1);
-
-	p = *head;
-	while (p != NULL)
-	{
-		p = p->next;
-		numNodes++;
-	}
-
-	s = malloc(sizeof(int) * numNodes);
-	if (s == NULL)
-		return (-1);
-
-	temp = *head;
-
-	while (temp != NULL)
-	{
-		s[i] = temp->n;
-		temp = temp->next;
-		i++;
-	}
-
-	if (is_palin(s) == 1)
-	{
-		free(s);
+	if ((*head)->next == NULL)
 		return (1);
+	while (current != NULL && aux != NULL && aux->next != NULL)
+	{
+		current = current->next;
+		aux = aux->next->next;
 	}
-	free(s);
-	return (0);
+	current = reverse_listint(&current);
+	aux = *head;
+	while (aux != NULL && current != NULL)
+	{
+		if (aux->n != current->n)
+			return (0);
+		aux = aux->next;
+		current = current->next;
+	}
+	return (1);
+}
+/**
+ * reverse_listint - Function that reverses a listint_t linked list
+ *
+ * @head: This is the input single linked list
+ *
+ * Return: Pointer to the first node of the reversed list
+ */
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *next = *head, *current = NULL;
+
+	while (*head != NULL)
+	{
+		next = (*head)->next;
+		(*head)->next = current;
+		current = *head;
+		*head = next;
+	}
+	*head = current;
+	return (*head);
 }
