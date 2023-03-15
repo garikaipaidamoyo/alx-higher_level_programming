e6ce4f26f77603409f5e8e59150a1afe1811968c
paidamoyo <garikaipaidamoyo@gmail.com>
2023-03-15 16:04:59 -0700

    Squared simple

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    matrix_square = []
-    for i in matrix:
-        matrix_square.append(list(map(lambda x: x * x, i))))
-    return matrix_square
+    return ([list(map(lambda x: x * x, row)) for row in matrix])
