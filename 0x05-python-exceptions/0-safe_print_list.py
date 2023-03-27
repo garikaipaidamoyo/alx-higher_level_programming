5931db73728e425d47b3871eb4cc38ed241962e9
paidamoyo <garikaipaidamoyo@gmail.com>
2023-03-27 10:19:18 -0700

    Safe list printing

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,6 +6,5 @@ def safe_print_list(my_list=[], x=0):
             print(mylist[i], end' ')
             count += 1
     except IndexError:
-        break
-    print("")
+        print("")
     return count
