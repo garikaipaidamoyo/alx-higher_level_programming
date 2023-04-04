59a15158c9f00d672b9e3cbd17f7971c2fdf6b8f
paidamoyo <garikaipaidamoyo@gmail.com>
2023-04-04 08:15:28 -0700

    Text indentation

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+a function that prints a text with 2 new lines after each of these characters: ., ? and :
+
+"""
 def text_indentation(text):
     # Check if text is a string
     if not isinstance(text, str):
