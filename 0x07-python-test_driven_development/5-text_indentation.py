701412b7b480c66a4e0de4bd0cbdf4e1a0a30b43
paidamoyo <garikaipaidamoyo@gmail.com>
2023-04-04 08:12:15 -0700

    Text indentation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def text_indentation(text):
+    # Check if text is a string
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    # Split the text into sentences using '.', '?' and ':' as delimiters
+    sentences = []
+    sentence = ""
+    for char in text:
+        sentence += char
+        if char in ".?:":
+            sentences.append(sentence.strip())
+            sentence = ""
+    if sentence:
+        sentences.append(sentence.strip())
+
+    # Print the sentences with 2 new lines after each sentence
+    for sentence in sentences:
+        print(sentence)
+        print()
