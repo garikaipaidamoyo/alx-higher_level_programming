7ee92af7a444fe6326dd90b1341e8cdd00953ec8
paidamoyo <garikaipaidamoyo@gmail.com>
2023-06-14 11:36:58 -0700

    Rectangle #3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+/*
+class rectangle contain height & width
+print the square with 'X'
+*/
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+};
