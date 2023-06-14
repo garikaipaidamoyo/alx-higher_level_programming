ff2f4afe85d162f53816e6d68ef22d4e3d4ef89f
paidamoyo <garikaipaidamoyo@gmail.com>
2023-06-14 11:34:04 -0700

    Rectangle #2

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// class rectangle contain height & width
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+};
